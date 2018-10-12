435eb0a855c45b1db22381a1ca2dd3fa00580ac9
Jared Reynolds <jared.reynolds@changepoint.com>
2018-10-12 00:55:26 -0700

    Exclude alias props and flatten to serialize enum

--- MgFwdConfig/Models/EmailFwd.cs
+++ MgFwdConfig/Models/EmailFwd.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 
 namespace MgFwdConfig.Models
@@ -13,12 +14,17 @@ namespace MgFwdConfig.Models
         public int Priority { get; set; }
         public RuleType RuleType { get; set; }
 
-        public string Domain { get { return PartitionKey; } set { PartitionKey = value; } }
-        public string LocalPart { get { return RowKey; } set { RowKey = value; } }
+        [IgnoreProperty] public string Domain { get { return PartitionKey; } set { PartitionKey = value; } }
+        [IgnoreProperty] public string LocalPart { get { return RowKey; } set { RowKey = value; } }
         public string SentTo => $"{LocalPart}@{Domain}";
 
         public EmailFwd() : base() { }
         public EmailFwd(string localPart, string domain) : base(localPart, domain) { }
+
+        public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            return Flatten(this, operationContext);
+        }
 
         public override string ToString()
         {
